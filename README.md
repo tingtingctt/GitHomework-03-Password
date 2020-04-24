9d8d5d5c0755dc4635dc8e85af3019037394b4d5
Tingting Chen <tingtingctt@gmail.com>
2020-04-23 20:06:55 -0700

    fully functioning

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # GitHomework-03-Password
-Random Passwork Generator
+User Guide:
+
+When the webpage is refreshed, the user will be prompted to choose their criteria to generate a password.
+
+The console log will record all the inputs of the user.
+
+After the alerts, user can press the "Generate Password" button to generate as many random passwords as he/she wants.
+
+
+
+Some functions I have used:
+
+To force the user to input a password length between 8 and 128, I used "while" function to loop until the user inputs a valid number.
+
+To force the user to choose at least one of the character options to inclue, I also used "while" function to loop until the user clicks at least one "ok".
+
+After the user has selected all criteria, the pool of password characters will be console.logged.
+
+
---
+++ script.js
@@ -0,0 +1,73 @@
+
+// To prompt the user to input a password-length number between 8 and 128.
+var userLength = prompt ("Choose your password length between 8 and 128.");
+
+while (userLength <8 || userLength >128){
+  alert ("Hey! You need to enter a value between 8 and 128!");
+  userLength = prompt ("Choose your password length between 8 and 128.");
+}
+
+console.log("Password Length: " + userLength);
+
+
+// To prompt the user to choose at least one of the character options.
+var userLower = confirm ("Would you like to include lower cases?");
+var userUpper = confirm ("Would you like to include upper cases?");
+var userNumber = confirm ("Would you like to include numbers?");
+var userSpecial = confirm ("Would you like to include special characters?");
+
+while (userLower===false && userUpper===false && userNumber===false && userSpecial===false){
+  alert ("Listen! You need to choose at least one of the options to include in your password!");
+  userLower = confirm ("Would you like to include lower cases?");
+  userUpper = confirm ("Would you like to include upper cases?");
+  userNumber = confirm ("Would you like to include numbers?");
+  userSpecial = confirm ("Would you like to include special characters?");
+}
+
+console.log("Lower Case: " + userLower + ";   " + "Upper Case: " + userUpper + ";   " + "Numbers: " + userNumber + ";   " + "Special Characters: " + userSpecial + ";   ");
+
+
+// To create a pool of password characters based on user's choice.
+var passwordPool = "";
+if (userLower){
+  passwordPool += "abcdefghijklmnopqrstuvwxyz";
+}
+if (userUpper){
+  passwordPool += "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+}
+if (userNumber){
+  passwordPool += "0123456789";
+}
+if (userSpecial){
+  passwordPool += " !#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
+}
+
+console.log("Password pool includes: " + passwordPool);
+
+
+// Using generatePassword() function to return a value to writePassword();
+function generatePassword(){
+  var userPassword = "";
+  for (var i=0 ; i < userLength; i++){
+    var num = Math.floor(Math.random() * passwordPool.length);
+    userPassword += passwordPool.charAt(num);
+  };
+  console.log(userPassword);
+  return userPassword;
+}
+
+
+
+
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+  passwordText.value = password;
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
